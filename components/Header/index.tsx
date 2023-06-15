fb6566843b86095581e32c5bcce672efe60a411c
Ilya <114355247+ipqw@users.noreply.github.com>
2023-06-15 11:42:06 +0400

    Initialize develop

---
+++ components/Header/index.tsx
@@ -0,0 +1,236 @@
+import { observer } from 'mobx-react';
+import Link from 'next/link';
+import { useLang } from 'store/lang';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import sun from '../../assets/icons/sun.svg';
+import { store } from '../../store';
+import { Content } from '../Content';
+import NavLink from '../NavLink';
+
+const closeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
+const burgerIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="18" x2="21" y2="18" />
+  </svg>
+);
+
+export const Header = observer(() => {
+  const lang = useLang();
+  const color = store.isDark ? 'white' : 'black';
+  const [matchesMobile, setMatchesMobile] = useState(false);
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+  useEffect(() => {
+    setMatchesMobile(window.matchMedia('(max-width: 580px)').matches);
+    window.matchMedia('(max-width: 580px)').addEventListener('change', e => {
+      setMatchesMobile(e.matches);
+    });
+  }, []);
+  const clickableContent = (
+    <>
+      <NavMenu>
+        <NavLink href="/" name={lang.navLinkMain} />
+        <NavLink href="/projects" name={lang.navLinkProjects} />
+      </NavMenu>
+      <Buttons>
+        <ThemeButton className="clickable" onClick={store.changeTheme}>
+          <Image src={sun.src} alt="" width={18} />
+        </ThemeButton>
+        <LangsWrapper>
+          <Langs
+            style={{ color: color }}
+            className="clickable"
+            onClick={store.changeLang}
+          >
+            {store.lang.toUpperCase()}
+          </Langs>
+        </LangsWrapper>
+      </Buttons>
+    </>
+  );
+  return (
+    <HeaderWrapper
+      style={{
+        backgroundColor: store.isDark ? 'rgba(255, 255, 255, 0.1)' : '#f5f5f5'
+      }}
+    >
+      <ContentHeader>
+        <Link href="/">
+          <Logo style={{ color: color }}>BetterWeb</Logo>
+        </Link>
+        {matchesMobile ? (
+          <>
+            <MenuIcon
+              onClick={() => {
+                setMobileMenuOpened(!mobileMenuOpened);
+                document.body.classList.toggle('no-scroll');
+              }}
+            >
+              {mobileMenuOpened ? closeIcon : burgerIcon}
+            </MenuIcon>
+            <ClickableContent
+              style={{ backgroundColor: store.isDark ? 'black' : '#d9d9d9' }}
+              opened={mobileMenuOpened}
+            >
+              {clickableContent}
+            </ClickableContent>
+          </>
+        ) : (
+          clickableContent
+        )}
+      </ContentHeader>
+    </HeaderWrapper>
+  );
+});
+
+const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100px;
+  z-index: 2;
+  backdrop-filter: blur(7px);
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
+const ContentHeader = styled(Content)`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 580px) {
+    padding: 20px;
+  }
+`;
+
+const NavMenu = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 580px) {
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+`;
+
+const ThemeButton = styled.button`
+  width: 3em;
+  height: 3em;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  border: none;
+  transition: ease-in-out 0.3s;
+  border-radius: 50%;
+
+  &:hover {
+    background-color: #ddd;
+    box-shadow: 0px 0px 2px 0px #aaa inset;
+  }
+`;
+
+const Langs = styled.p`
+  @media (max-width: 580px) {
+    margin: 0;
+  }
+`;
+
+const LangsWrapper = styled.div`
+  width: 30px;
+`;
+
+const Logo = styled.p`
+  font-family: Montserrat, OpenSans, sans-serif;
+  font-weight: 700;
+  font-size: 1.75rem;
+
+  @media (max-width: 580px) {
+    margin: 0;
+  }
+`;
+
+interface mobileMenuProps {
+  opened: boolean;
+}
+
+const ClickableContent = styled.div<mobileMenuProps>`
+  display: flex;
+  align-items: center;
+  background-color: rgba(26, 26, 26, 1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
+  transform: ${props => (props.opened ? 'none' : 'translateX(-100vw)')};
+  transition: transform 0.3s ea;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  will-change: transform, opacity;
+  transition: transform 0.4s cubic-bezier(0, 0, 0.3, 1);
+  z-index: 5;
+  @media (max-width: 375px) {
+    flex-direction: column;
+  }
+`;
+
+const MenuIcon = styled.div`
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  z-index: 6;
+  & svg {
+    color: #fff;
+    @media (prefers-color-scheme: dark) {
+      color: #fff;
+    }
+    width: 100%;
+    height: 100%;
+  }
+`;
+const Image = styled.img`
+  margin: 0;
+  object-fit: cover;
+  color: #fff;
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
+`;
---
+++ store/lang/en.ts
@@ -0,0 +1,27 @@
+import { Lingo } from './ru';
+
+export const en: Lingo = {
+  navLinkMain: 'Main',
+  navLinkProjects: 'Projects',
+  headingOurTeam: 'Our team',
+  headingAdresses: 'Adresses',
+  footerRights: 'All rights reserved © 2022',
+  footerContactUs: 'Contact us',
+  popupStatusInProgress: 'Sending…',
+  popupStatusDone: 'Sent',
+  popupStatusError: 'Error. Try again?',
+  popupButton: 'Send',
+  popupHeading: 'Contact us',
+  popupName: 'Name: ',
+  popupMessage: 'Message: ',
+  popupConditions: 'I agree to the processing of personal data',
+  projectDescription: 'Description: ',
+  dateCreated: 'Date of creation: ',
+  projectStack: 'Project stack: ',
+  notMemberError: 'There is not member with same id!',
+  meOnMap: 'Me on map',
+  myContacts: 'My contacts',
+  myStack: 'My stack',
+  showMore: 'Show more...',
+  allCategories: 'Any stack'
+};
