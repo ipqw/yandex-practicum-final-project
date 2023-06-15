fffc0f9a6a00b2f1f1854c590a79d7b268a3cb43
Ilya <114355247+ipqw@users.noreply.github.com>
2023-06-15 11:41:49 +0400

    Initialize footer_new

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Page from '../components/Page';
+import styled from 'styled-components';
+
+export default function Home() {
+  return (
+    <Page>
+      <p>content...</p>
+    </Page>
+  );
+}
